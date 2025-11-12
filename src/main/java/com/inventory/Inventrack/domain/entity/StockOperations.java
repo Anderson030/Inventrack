a11f5508e2ea2b341fe5786098406accc71b272d
Andrsxfelipe <afelipelondono81@gmail.com>
2025-11-12 11:41:09 -0500

    Refactor StockOperations entity to use Lombok annotations and remove boilerplate code

--- src/main/java/com/inventory/Inventrack/domain/entity/StockOperations.java
+++ src/main/java/com/inventory/Inventrack/domain/entity/StockOperations.java
@@ -1,11 +1,17 @@
 package com.inventory.Inventrack.domain.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "stock_operations")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class StockOperations {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,50 +19,8 @@ public class StockOperations {
     @ManyToOne
     @JoinColumn(name = "product_id")
     private Product product;
-    private String operation_type; // "IN" or "OUT"
+    private String operation_type;
     private Integer quantity;
     private String reason;
     private LocalDateTime operation_date;
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Product getProduct() {
-        return product;
-    }
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
-    public String getOperation_type() {
-        return operation_type;
-    }
-    public void setOperation_type(String operation_type) {
-        this.operation_type = operation_type;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
-    public String getReason() {
-        return reason;
-    }
-    public void setReason(String reason) {
-        this.reason = reason;
-    }
-
-    public LocalDateTime getOperation_date() {
-        return operation_date;
-    }
-    public void setOperation_date(LocalDateTime operation_date) {
-        this.operation_date = operation_date;
-    }
 }
