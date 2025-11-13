cc79039c4451a5a85458243f2ac869d1e3d173f9
Ander <anderson.blandon@hotmail.com>
2025-11-12 21:01:43 -0500

    Revert "Merge pull request #13 from Anderson030/feature/api-endpoints"
    This reverts commit 1c1d7bba01f82703ff715087d58aa9f3b6b7c091, reversing
    changes made to 366b69367cd028e49fc07b93f5d421cb04a5a558.

--- src/main/java/com/inventory/Inventrack/domain/dto/PurchaseRequestDTO.java
+++
@@ -1,8 +0,0 @@
-package com.inventory.Inventrack.domain.dto;
-
-import java.time.LocalDateTime;
-
-public class PurchaseRequestDTO {
-    private String dealer;
-    private Double total_cost;
-}
--- src/main/java/com/inventory/Inventrack/domain/dto/UserRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package com.inventory.Inventrack.domain.dto;
-
-import java.time.LocalDateTime;
-
-public class UserRequestDTO {
-    private String name;
-    private String hash_password;
-    private String role;
-}
